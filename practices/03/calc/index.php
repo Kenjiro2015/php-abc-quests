c8f67f106ad27300738911f8693caa6145db8b6f
Kenjirou2015 <muscle_body2004@hotmail.com>
2015-02-15 08:34:40 +0900

    03 Calc

--- practices/03/calc/index.php
+++ practices/03/calc/index.php
@@ -1,5 +1,9 @@
 <?php
-$answer = 100;
+if(isset($_GET['left'])){
+	$answer = $_GET['left'];
+}else{
+	$answer = 'No Result';
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,14 +12,17 @@ $answer = 100;
 	<title>test</title>
 </head>
 <body>
-<input type="text" name="left" required autofocus/>
-<select name="operator">
-	<option valuse="+" selected>+</option>
-	<option valuse="-">-</option>
-	<option valuse="*">*</option>
-	<option valuse="/">/</option>
-</select>
-<input type="text" name="right" required/>
+<form action = "index.php" method = "GET">
+	<input type="text" name="left" required autofocus/>
+	<select name="operator">
+		<option valuse="+" selected>+</option>
+		<option valuse="-">-</option>
+		<option valuse="*">*</option>
+		<option valuse="/">/</option>
+	</select>
+	<input type="text" name="right" required/>
+	<input type="submit" value="Calc">
+</form>
 <p><?php echo $answer; ?></p>
 </body>
 </html>
